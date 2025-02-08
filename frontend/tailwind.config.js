677e7bd928703bdc89835c2a44c85116f6ba15e1
Felipe Mascarenhas <felipefmascarenhas31@gmail.com>
2025-02-08 18:54:19 -0300

    Configurando ambiente, falta instalar o Tailwind corretamente

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/index.html",  // Certifique-se de que o arquivo HTML seja incluído
+    "./src/**/*.{js,jsx,ts,tsx}",  // Incluir todos os arquivos JavaScript/JSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
